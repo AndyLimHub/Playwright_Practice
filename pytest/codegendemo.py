310824406ea9bf687b264e47af3a27ab9594ecc6
Andrew Lim <Andrew.Lim@e14.io>
2025-02-12 12:11:47 -0300

    Installed playwright sidebar tool and added some config files

--- pytest/codegendemo.py
+++ pytest/codegendemo.py
@@ -4,10 +4,11 @@ from playwright.sync_api import Page, expect
 
 def test_example(page: Page) -> None:
     page.goto("https://www.saucedemo.com/")
+    page.pause()
     page.locator("[data-test=\"username\"]").click()
     page.locator("[data-test=\"username\"]").fill("standard_user")
     page.locator("[data-test=\"username\"]").press("Tab")
     page.locator("[data-test=\"password\"]").fill("secret_sauce")
     page.locator("[data-test=\"login-button\"]").click()
     expect(page.locator("#inventory_container").first).to_be_visible()
-    expect(page).to_have_url(re.compile(r".*/inventory\.html"))
+    expect(page).to_have_url(re.compile(r".*/inventory\.html")) 
