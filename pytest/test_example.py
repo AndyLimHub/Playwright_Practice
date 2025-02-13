ea8622bc6dfe867d0fa95e7adf5bef6e128686db
Andrew Lim <Andrew.Lim@e14.io>
2025-02-13 11:51:06 -0300

    added **.pyc to gitignore

--- pytest/test_example.py
+++ pytest/test_example.py
@@ -1,7 +1,6 @@
 import re
 from playwright.sync_api import Page, expect
 
-
 def test_example(page: Page) -> None:
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.pause()
---
+++ pytest/test_my_application.py
@@ -0,0 +1,4 @@
+def test_my_app_is_working(new_context):
+    pass
+    # Test using fixture_name
+    # ...
